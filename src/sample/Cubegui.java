10ca249b113115db6bda2d926082a340e794f83c
Robin-LESAOUT <65037222+Robin-LESAOUT@users.noreply.github.com>
2020-06-17 10:47:49 +0200

    ajout des classes java FX au projet , init de la partie javaFX

---
+++ src/sample/Cubegui.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Cubegui extends Application {
+    @Override
+    public void start (Stage primaryStage) {
+        try {
+            Parent content = FXMLLoader.load(getClass().getResource("gui2D.fxml"));
+            primaryStage.setTitle("Cube 3D in GUI 2D");
+            primaryStage.setScene(new Scene(content));
+            primaryStage.show();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
